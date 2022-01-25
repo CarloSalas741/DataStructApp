7d32bc82e6db5cea56313af9514e19b3ccf907b4
CarloSalas741 <c.salas.denegri@gmail.com>
2022-01-25 15:05:04 -0500

    xd

---
+++ public/js/queue.js
@@ -0,0 +1,72 @@
+var btnQueue = document.getElementById("btn-queue");
+var btnEnqueue = document.getElementById("btn-enqueue");
+var btnPeek = document.getElementById("btn-peek");
+var container = document.getElementById("elements-container");
+
+var addNode = function addNode() {
+  var containerLength = container.children.length;
+  var txtData = document.getElementById("txt");
+
+  if (!isEmpty(txtData.value)) {
+    container.innerHTML += createElement(txtData.value);
+
+    if (containerLength == 0) {
+      container.firstElementChild.firstElementChild.remove();
+    }
+
+    AddAnimation(container);
+  } else {
+    errorMessage("data field is required");
+    txtData.classList.add("isEmpty");
+    setTimeout(function () {
+      txt.classList.remove("isEmpty");
+    }, 1000);
+  }
+};
+
+var createElement = function createElement(data) {
+  return "\n   <div class=\"element d-flex\">\n          <div class=\"arrow-left\"><img class=\"right\" src=\"resources/svg/down-arrow.svg\"></div>\n          <div class=\"data rounded-circle\">".concat(data, "</div>\n    </div>\n    ");
+};
+
+var removeNode = function removeNode() {
+  var element = container.firstElementChild;
+  element.animate([{
+    transform: "scale(1)"
+  }, {
+    transform: "scale(0)"
+  }], {
+    duration: 500
+  });
+  setTimeout(function () {
+    element.remove();
+    element = container.firstElementChild;
+    element.firstElementChild.remove();
+  }, 500);
+};
+
+var peekNode = function peekNode() {
+  var element = container.firstElementChild.lastElementChild;
+  element.animate([{
+    backgroundColor: "#00c261"
+  }, {
+    backgroundColor: "#f2ff3e"
+  }], {
+    duration: 1000,
+    iterations: 2
+  });
+};
+
+var AddAnimation = function AddAnimation(element) {
+  var divData = element.lastElementChild;
+  divData.animate([{
+    transform: "scale(0)"
+  }, {
+    transform: "scale(1)"
+  }], {
+    duration: 500
+  });
+};
+
+btnQueue.addEventListener('click', addNode);
+btnEnqueue.addEventListener('click', removeNode);
+btnPeek.addEventListener('click', peekNode);
---
+++ public/js/stack.js
@@ -0,0 +1,78 @@
+var btnPush = document.getElementById("btn-push");
+var btnPop = document.getElementById("btn-pop");
+var btnPeek = document.getElementById("btn-peek");
+var container = document.getElementById("elements-container");
+
+var addNode = function addNode() {
+  var containerLength = container.children.length;
+  var txtData = document.getElementById("txt");
+
+  if (!isEmpty(txtData.value)) {
+    if (containerLength > 0) {
+      var lastElement = container.lastElementChild;
+      lastElement.animate([{
+        marginTop: "0px"
+      }, {
+        marginTop: "160px"
+      }], {
+        duration: 800
+      });
+    }
+
+    setTimeout(function () {
+      container.innerHTML += createElement(txtData.value);
+      AddAnimation(container);
+    }, containerLength > 0 ? 800 : 0);
+  } else {
+    errorMessage("data field is required");
+    txtData.classList.add("isEmpty");
+    setTimeout(function () {
+      txt.classList.remove("isEmpty");
+    }, 1000);
+  }
+};
+
+var createElement = function createElement(data) {
+  return "\n    <div class=\"element\">\n        <div class=\"data rounded-circle\">".concat(data, "</div>\n        <div class=\"arrow\"><img src=\"resources/svg/down-arrow.svg\"></div>\n    </div>");
+};
+
+var removeNode = function removeNode() {
+  var element = container.lastElementChild;
+  element.animate([{
+    transform: "scale(1)"
+  }, {
+    transform: "scale(0)"
+  }], {
+    duration: 500
+  });
+  setTimeout(function () {
+    element.remove();
+  }, 500);
+};
+
+var peekNode = function peekNode() {
+  var element = container.lastElementChild.firstElementChild;
+  element.animate([{
+    backgroundColor: "#00c261"
+  }, {
+    backgroundColor: "#f2ff3e"
+  }], {
+    duration: 1000,
+    iterations: 2
+  });
+};
+
+var AddAnimation = function AddAnimation(element) {
+  var divData = element.lastElementChild;
+  divData.animate([{
+    transform: "scale(0)"
+  }, {
+    transform: "scale(1)"
+  }], {
+    duration: 500
+  });
+};
+
+btnPush.addEventListener('click', addNode);
+btnPop.addEventListener('click', removeNode);
+btnPeek.addEventListener('click', peekNode);
