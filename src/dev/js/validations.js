8fcef7d4c674ff318b6a1ba8aec9f6230c02c092
CarloSalas741 <49353829+CarloSalas741@users.noreply.github.com>
2022-01-25 14:44:12 -0500

    Delete src/dev directory

--- src/dev/js/validations.js
+++
@@ -1,9 +0,0 @@
-const isEmpty = string => string.length == 0;  
-
-const errorMessage = textMessage =>{
-    error = document.querySelector(".error-block");
-    error.classList.remove("invisible");
-    message = document.querySelector(".error-message");
-    message.textContent = textMessage;
-    setTimeout(() =>{error.classList.add("invisible") },5000);
-}
